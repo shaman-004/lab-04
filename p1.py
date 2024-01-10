8419bff946745c9cf21ed4179ffcebdb9a0b823b
shaman <shamanbusiness2004@gmail.com>
2024-01-10 11:24:40 +0530

    commited

---
+++ p1.py
@@ -0,0 +1,7 @@
+#write a python program to print the following string in a specific format?   
+# Twinkle, twinkle, little star,    
+def print_reverse(s):
+    return s[::-1]
+
+string = "Hello World!"
+print(print_reverse(string))
